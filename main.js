2a9eb6da16d105968d5256c69aa32cdfdaf1dbcc
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-07-11 14:44:32 -0400

    Added pseudocode

---
+++ main.js
@@ -0,0 +1,9 @@
+//When start button is pushed
+  //start the counter
+  //change the start button to green
+
+//start animation
+//if user successfully clicks the colors in the same order as shown, update counter
+  //else, restart counter & change start button to red
+
+//If user hits start button while it's green, stop it and turn it red
