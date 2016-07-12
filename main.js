be2ac57af2ca3e8c4e7bf78031928320981c389b
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-07-12 15:50:55 -0400

    Change the color of the start button when game is start or stopped

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
   console.log("Hello Moto");
   var start = $('#start');
+  var button = $('.button');
+  var red = $('#red');
+  var blue = $('blue');
+  var green = $('green');
+  var yellow = $('yellow');
   //When start button is pushed
   //start the counter
   //change the start button to green
-  $(start).on("click", function() {
-    console.log("start button clicked");
-  //  if ($(this).css("background-color") == "red") {
+  start.on("click", function() {
+    //console.log("start button clicked");
+    //Check Background color
+    //console.log( $(this).css("background-color") );
+    if ($(this).css("background-color") == "rgb(255, 0, 0)") {
       $("#start").css("background-color", "green");
-  /*  }
-      else {
-        console.log("didn't work");
-      }
-      */
+      console.log("Start");
+    } else {
+      console.log("Stop");
+      $("#start").css("background-color", "red");
+    }
+
   });
 
   //start animation
