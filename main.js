6b156a17f92112d2fd7c08b5a472da7bfe5ed864
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-07-13 09:38:33 -0400

    Added a way to keep track of the moves the computer & player make, how many rounds have been played and a function to reset the computers memory

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
-  console.log("Hello Moto");
+  console.log("Let's Play Simon!");
   var start = $('#start');
   var button = $('.button');
-  var red = $('#red');
-  var blue = $('blue');
-  var green = $('green');
-  var yellow = $('yellow');
+  //Keep track of the moves the computer makes
+  var sequence = [];
+  //Keep track of the moves the player makes
+  var copy = [];
+  //Keep track of how many rounds have been played
+  var round = 0;
+
   //When start button is pushed
   //start the counter
   //change the start button to green
@@ -14,18 +17,59 @@ $(document).ready(function() {
     //Check Background color
     //console.log( $(this).css("background-color") );
     if ($(this).css("background-color") == "rgb(255, 0, 0)") {
-      $("#start").css("background-color", "green");
+      //reset the moves from the computer, the moves from the player, and the round to 0
+      sequence = [];
+      copy = [];
+      round = 0;
+      //Change the start button to green when clicked
+      $("#start").css("background-color", "green").html("<p>Stop</p>");
       console.log("Start");
+      newRound();
     } else {
-      console.log("Stop");
-      $("#start").css("background-color", "red");
+      //console.log("Stop");
+      $("#start").css("background-color", "red").html("<p>Start</p>");
     }
-
   });
 
-  //start animation
-  //if user successfully clicks the colors in the same order as shown, update counter
-  //else, restart counter & change start button to red
+  //create an array (sequence) to assign the colors to numbers
+  /*function powerUp() {
 
-  //If user hits start button while it's green, stop it and turn it red
-})
+  //send array to the animate function
+  animate(sequence);
+}
+
+
+//start animation (simulate lighting by changing background opacity)
+function animate(sequence){
+  //keep calling this animation until finished sorting through the array
+  //regulate the timing of when the items light up one at a time
+  var interval = setInterval(function () {
+
+    //increment to track the progress over the array sequence
+    //start the lightButton function while the current color is in sequence
+    for(var i=0; i<=sequence.length; i++) {
+      darkButton(sequence[i]);
+
+      if(sequence.length>=i){
+        console.log(sequence[i]);
+        clearInterval(interval);
+      }
+    }
+  }, 600);
+}
+
+
+function darkButton(sequence) {
+  animate(sequence);
+  button.animate({opacity: 0.7}, 300);
+  button.animate({opacity: 1}, 300);
+  //setTimeout needed
+
+}
+*/
+
+//if user successfully clicks the colors in the same order as shown, update counter
+//else, restart counter & change start button to red
+
+//If user hits start button while it's green, stop it and turn it red
+});
