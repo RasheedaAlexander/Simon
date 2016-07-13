abda7ced77be61d90a54875d170669b4ecf11d03
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-07-13 17:53:50 -0400

    This code doesn't work. Starting over, starting simple

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
   console.log("Let's Play Simon!");
-  var start = $('#start');
-  var button = $('.button');
   //Keep track of the moves the computer makes
   var sequence = [];
   //Keep track of the moves the player makes
@@ -28,15 +26,15 @@ $(document).ready(function() {
     //when a new round starts is add 1 to the 'round' variable, which is keeping track of which round we are on
     //use .text() to set this round
     $('data-round').text(++this.round);
-    //let the computer randomly pick one of the colors
-    //send it to the sequence array to compare it to the color the user clicks
+    //let the computer randomly pick one of the tiles
+    //send it to the sequence array to compare it to the tile the user clicks
     sequence.push(randomNumber());
     //Keep track of the most recent input made by the computer
     //.slice() extracts a section of sequence and returns a new array
     copy = sequence.slice(0);
   }
   function animate(sequence) {
-    var i = 0;
+    var i = 0;2
     var interval = setInterval(function() {
       lightUp(sequence[i]);
       console.log(sequence[i] + ": sequence[i]");
@@ -48,7 +46,7 @@ $(document).ready(function() {
     }, 600);
   }
 
-  //light up colors
+  //light uptiles
   function lightUp(tile) {
     console.log("tile# passed into lightUp " + tile);
     console.log(tile);
@@ -74,35 +72,35 @@ $(document).ready(function() {
 
 
   function randomNumber(){
-    // between 1 and 4
+    // return a # between 1 and 4
     return Math.floor((Math.random() * 4) + 1);
   }
-  //Compare most recent input by computer to the color the user clicks
-  //Run this function when a user clicks a color
+  //Compare most recent input by computer to the tile the user clicks
+  //Run this function when a user clicks a tile
   function registerClick(evt) {
-    //check whether or not the user clicked the right color
+    //check whether or not the user clicked the right tile
     //desiredResponse(same as computer)
     //actualResponse(users response)
     //remove 1st item of sequence w/ .shift()
     var desiredResponse = this.copy.shift();
-    //get the evt element that triggered the response & attach color data to it
+    //get the evt element that triggered the response & attach tile data to it
     var actualResponse = $(evt.target).data('tile');
 
-    //compare the color clicked to the most recent move made by computer
-    //if input was correct, set to tru
+    //compare thetile clicked to the most recent move made by computer
+    //if input was correct, set to true
     //else, set to false
     correct = (desiredResponse === actualResponse);
   }
   //
   function checkLose(){
-    // Whenever a player clicks on a color, there are three possible outcomes
-    // 1.) User clicked the right color, but is yet to complete the pattern
-    // 2.) User clicked the right color, and it was also the last color in the pattern
-    // 3.) User clicked on the wrong color, and the game ends
+    // Whenever a player clicks on a tile, there are three possible outcomes
+    // 1.) User clicked the right tile, but is yet to complete the pattern
+    // 2.) User clicked the right tile, and it was also the last tile in the pattern
+    // 3.) User clicked on the wrong tile, and the game ends
     // This 'if statment' will only run if the user has completed the pattern correctly
     // First condition: check to see how many moves are left in the pattern
     //If there are 0 moves left, move on to the next condition
-    // Second condition: check to see if the color the user clicked on was the correct color
+    // Second condition: check to see if the tile the user clicked on was the correcttile
     //If it was, the function will run!
     if (this.copy.length === 0 && this.active) {
       //disable game board once the round is over
